2a3352fe0c96fedf3b5ca40a15dd86560db276fc
Jason Saenz <jsigns@gmail.com>
2018-06-10 13:29:57 -0500

    couple things

--- src/components/NavMobile/NavMob.js
+++ src/components/NavMobile/NavMob.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Button from "../Button";
-import { Collapse } from "reactstrap";
+import { Collapse, CardBody, Card } from "reactstrap";
 
 class NavMob extends Component {
   constructor(props) {
@@ -16,43 +16,43 @@ class NavMob extends Component {
   render() {
     return (
       <div className="topNav navbar-right">
-        <Button
-        id="burger"
-        onClick={this.toggle}
-        style={{float: "right"}}
-        >
+        <Button id="burger" onClick={this.toggle} style={{ float: "right" }}>
           <i className="fa fa-bars" />
         </Button>
         <Collapse isOpen={this.state.collapse}>
-          <div className="navbar-collapse" id="navbar-collapse-1">
-            <ul className="nav navbar-nav navbar-right">
-              <li key={10}>
-                {" "}
-                <Button id="btn1" onClick={this.props.homeButton}>
-                  Home
-                </Button>
-              </li>
-              <li key={6}>
-                {" "}
-                <Button id="btn2" onClick={this.props.techButton}>
-                  Technical Facts
-                </Button>
-              </li>
-              <li key={7}>
-                {" "}
-                <Button id="btn3" onClick={this.props.faqButton}>
-                  FAQ
-                </Button>
-              </li>
-              <li key={8}>
-                <Button id="btn4">Photo Gallery</Button>
-              </li>
-              <li key={9}>
-                {" "}
-                <Button id="btn5">Video Gallery</Button>
-              </li>
-            </ul>
-          </div>
+          <Card>
+            <CardBody>
+              <div className="navbar-collapse" id="navbar-collapse-1">
+                <ul className="nav navbar-nav navbar-right">
+                  <li key={10}>
+                    {" "}
+                    <Button id="btn1" onClick={this.props.homeButton}>
+                      Home
+                    </Button>
+                  </li>
+                  <li key={6}>
+                    {" "}
+                    <Button id="btn2" onClick={this.props.techButton}>
+                      Technical Facts
+                    </Button>
+                  </li>
+                  <li key={7}>
+                    {" "}
+                    <Button id="btn3" onClick={this.props.faqButton}>
+                      FAQ
+                    </Button>
+                  </li>
+                  <li key={8}>
+                    <Button id="btn4">Photo Gallery</Button>
+                  </li>
+                  <li key={9}>
+                    {" "}
+                    <Button id="btn5">Video Gallery</Button>
+                  </li>
+                </ul>
+              </div>
+            </CardBody>
+          </Card>
         </Collapse>
       </div>
     );
